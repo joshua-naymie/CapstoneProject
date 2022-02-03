42083e558ed4c25b6062287a480a79493aa1ea4b
David Wei <yunze.wei@edu.sait.ca>
2022-02-03 15:53:18 -0700

    Commit 9
    Set up JPA  / DB connection

--- src/java/servlets/AccountServlet.java
+++ src/java/servlets/AccountServlet.java
@@ -4,12 +4,16 @@
  */
 package servlets;
 
+import dataaccess.DBUtil;
+import jakarta.persistence.EntityManager;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.*;
+import models.User;
 
 /**
  *
@@ -22,7 +26,13 @@ public class AccountServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        EntityManager entityManager = DBUtil.getEMFactory().createEntityManager();
+        List<User> test = entityManager.createNamedQuery("User.findAll").getResultList();
+        User user = test.get(0);
+        request.setAttribute("user", user);
         getServletContext().getRequestDispatcher("/WEB-INF/User.jsp").forward(request, response);
+        
     }
 
    
