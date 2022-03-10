1606efc798c5b1d52904e167e01405e6bb2ca18b
Li <xiaomeng.li@edu.sait.ca>
2022-03-10 15:46:40 -0700

    Front End Update: Integration of front and backend for editTask page

--- web/scripts/editTask.js
+++ web/scripts/editTask.js
@@ -7,29 +7,24 @@
 //		"notes":null,
 //		"task_description":null,
 //		"task_city":Calgary};
-window.onload = () =>
-{
-	console.log(editTask);
-	let task_desc = document.getElementById('task_description');
-	task_desc.setAttribute("value", editTask.task_description);
-	
-	let task_program = document.getElementById('task_program');
-	task_program.setAttribute("value", editTask.program_name);
-	
-	let task_city = document.getElementById('task_city');
-	task_city.setAttribute("value", editTask.task_city);
-	
-	let task_date = document.getElementById('task_date');
-	let date_string = new Date(editTask.start_time).toISOString().substring(0, 10);
-	task_date.setAttribute("value", date_string);
-	
-	let task_start_time = document.getElementById('task_start_time');
-	let start_time_string = new Date(editTask.start_time).toLocaleTimeString().substring(0, 7);
-	console.log(start_time_string);
-	task_start_time.setAttribute("value", start_time_string);
-	
-	let task_end_time = document.getElementById('task_end_time');
-	task_end_time.setAttribute("value", editTask.task_end_time);
-	
-	
-}
+window.onload = () => {
+  console.log(editTask);
+  let task_desc = document.getElementById("task_description");
+  task_desc.setAttribute("value", editTask.task_description);
+
+  let task_program = document.getElementById("task_program");
+  task_program.setAttribute("value", editTask.program_name);
+
+  let task_city = document.getElementById("task_city");
+  task_city.setAttribute("value", editTask.task_city);
+
+  let task_date = document.getElementById("task_date");
+  task_date.setAttribute("value", editTask.date);
+
+  let task_start_time = document.getElementById("task_start_time");
+  //   let start_time_string = new Date(editTask.start_time).toLocaleTimeString().substring(0, 7);
+  task_start_time.setAttribute("value", editTask.start_time);
+
+  let task_end_time = document.getElementById("task_end_time");
+  task_end_time.setAttribute("value", editTask.end_time);
+};
