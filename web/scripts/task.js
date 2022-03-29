b08bac73e7b16bf863d31e38024929e8c654021f
Li <xiaomeng.li@edu.sait.ca>
2022-03-29 15:39:16 -0600

    FrontEnd Update: Completed refactoring code for view task page

--- web/scripts/task.js
+++ web/scripts/task.js
@@ -2,92 +2,6 @@ window.onload = () => {
   const bodyNode = document.getElementById("main");
 
   bodyNode.appendChild(buildWeekList());
-
-  //   // accordionBody.appendChild(accordionBodyRow);
-  //   let groupIdSet = new Set();
-
-  //   console.log(taskDataSet);
-  //   let taskData = taskDataSet.filter((task) => {
-  //     let taskTime = new Date(task.start_time);
-  //     return taskTime === i;
-  //   });
-
-  //   console.log(taskData);
-  //   let groupId = taskData.group_id;
-  //   let program_id = taskData.program_id;
-
-  //   if (!groupIdSet.has(groupId)) {
-  //     // if (taskTime.getWeek() === i) {
-  //     // Append header to the table if there is data
-  //     table.appendChild(thead);
-
-  //     let showEditButton = JSON.parse(taskData.show_edit);
-  //     if (showEditButton) {
-  //       let td_edit_button = document.createElement("td");
-  //       td_edit_button.appendChild(accordionEditButton);
-  //       tr.appendChild(td_edit_button);
-  //     }
-
-  //     let showSignUpButton = JSON.parse(taskData.show_signupT_cancelF);
-  //     if (showSignUpButton) {
-  //       let td_signup_button = document.createElement("td");
-  //       td_signup_button.appendChild(accordionSignupButton);
-  //       tr.appendChild(td_signup_button);
-  //     }
-
-  //     let showCancelButton =
-  //       !JSON.parse(taskData.show_signupT_cancelF) &&
-  //       JSON.parse(taskData.can_cancel);
-  //     if (showCancelButton) {
-  //       let td_cancel_button = document.createElement("td");
-  //       td_cancel_button.appendChild(accordionCancelButton);
-  //       tr.appendChild(td_signup_button);
-  //     }
-
-  //     tbody.appendChild(tr);
-  //   }
-
-  //   groupIdSet.add(groupId);
-  // }
-  // else if (
-  //   program_id &&
-  //   program_id === "1" &&
-  //   groupId &&
-  //   groupIdSet.has(groupId)
-  // ) {
-  //   let parent = document.querySelector(`#group_id${groupId}`);
-  //   // console.log(parent);
-  //   // console.log(document.querySelector(`#group_id${groupId}`));
-
-  //   if (parent) {
-  //     let accordionItem = document.createElement("div");
-  //     accordionItem.className = "accordion-item";
-  //     let h2 = document.createElement("h2");
-  //     h2.className = "accordion-header";
-  //     let button = document.createElement("button");
-  //     button.className = "accordion-button";
-  //     button.type = "button";
-  //     button.setAttribute("data-bs-toggle", "collapse");
-  //     button.setAttribute("data-bs-target", "#taskcollapse" + groupId);
-  //     button.setAttribute("aria-expanded", "true");
-  //     button.setAttribute("aria-controls", "taskcollapse" + groupId);
-  //     button.innerHTML = parent;
-  //     h2.append(button);
-  //     accordionItem.append(h2);
-
-  //     let i = 0;
-  //     while ((parent = parent.previousSibling) != null) {
-  //       i++;
-  //     }
-  //     console.log(i);
-
-  //     let tbody = document.querySelector("tbody");
-  //     console.log(tbody.childNodes[i]);
-  //     tbody.replaceChild(accordionItem, tbody.childNodes[i]);
-  //   }
-  // }
-
-  // table.appendChild(tbody);
 };
 
 function onEdit(task_id) {
@@ -174,7 +88,7 @@ function buildWeekList() {
   return weekList;
 }
 
-function buildAccordionHeader(id, collapased, headerText) {
+function buildAccordionHeader(id, collapased, headerContent, isNode = false) {
   let accordionHeader = document.createElement("h2");
 
   //		Accordion header
@@ -190,7 +104,11 @@ function buildAccordionHeader(id, collapased, headerText) {
   accordionButton.setAttribute("data-bs-target", "#collapse" + id);
   accordionButton.setAttribute("aria-expanded", "true");
   accordionButton.setAttribute("aria-controls", "collapse" + id);
-  accordionButton.innerText = headerText;
+  if (isNode) {
+    accordionButton.appendChild(headerContent);
+  } else {
+    accordionButton.innerText = headerContent;
+  }
 
   accordionHeader.appendChild(accordionButton);
 
@@ -213,6 +131,7 @@ function buildAccordionBody(id, collapase, tasksInWeek) {
     { colName: "Spots Available" },
     { colName: "Operation", colspan: 3 },
   ];
+
   let table = buildTable(headerNames, tasksInWeek);
   accordionBody.appendChild(table);
 
@@ -292,13 +211,122 @@ function buildTableHeader(colNames) {
 function buildTableBody(taskList) {
   // ------------------------- Table Body ----------------------------
   let tbody = document.createElement("tbody");
+  let groupIdMap = new Map();
 
   taskList.forEach((task) => {
-    let tr = buildTableRow(task);
-    tbody.appendChild(tr);
+    // First run through task list find unique group_id
+    if (!groupIdMap.has(task.group_id)) {
+      groupIdMap.set(task.group_id, 1);
+    } else {
+      groupIdMap.set(task.group_id, groupIdMap.get(task.group_id) + 1);
+    }
   });
 
+  // Second run through find all tasks under one group_id, build table
+  groupIdMap.forEach((value, uniqueGroupId) => {
+    if (value > 1) {
+      let tasks = taskList.filter((task) => task.group_id == uniqueGroupId);
+
+      let groupTr = buildAccordionTask(tasks);
+      tbody.appendChild(groupTr);
+    } else {
+      let tasks = taskList.filter((task) => task.group_id == uniqueGroupId);
+      let tr = buildTableRow(tasks[0]);
+      tbody.appendChild(tr);
+    }
+  });
+
+  // console.log(groupIdMap);
   return tbody;
+}
+
+function buildAccordionTask(tasks) {
+  let tr = document.createElement("tr");
+  let task = tasks[0];
+  tr.className = "text-center";
+  tr.setAttribute("id", `group_id${task.group_id}`);
+  tr.className = "accordion";
+
+  let td = document.createElement("td");
+  td.setAttribute("colspan", 9);
+
+  let table = document.createElement("table");
+  table.className = "table mb-0";
+  let tbody = document.createElement("tbody");
+  let taskTr = buildTableRow(task);
+  tbody.appendChild(taskTr);
+  table.appendChild(tbody);
+
+  let accordionItem = document.createElement("div");
+  accordionItem.className = "accordion-item";
+
+  // ----------------- Task as Accordion Header: START ---------------//
+  let accordionHeader = document.createElement("h2");
+  accordionHeader.className = "accordion-header";
+  accordionHeader.id = `grouptask_heading${task.group_id}`;
+
+  let accordionButton = document.createElement("button");
+  let buttonCollapsed = "collapase";
+  accordionButton.className = "accordion-button " + buttonCollapsed;
+  accordionButton.type = "button";
+  accordionButton.setAttribute("data-bs-toggle", "collapse");
+  accordionButton.setAttribute(
+    "data-bs-target",
+    "#collapse_taskgroup" + task.group_id
+  );
+  accordionButton.setAttribute("aria-expanded", "true");
+  accordionButton.setAttribute(
+    "aria-controls",
+    "collapse_taskgroup" + task.group_id
+  );
+
+  // Build up DOM
+  accordionButton.appendChild(table);
+  accordionHeader.appendChild(accordionButton);
+  // accordionHeader.appendChild(taskTr);
+  accordionItem.appendChild(accordionHeader);
+  // ----------------- Task as Accordion Header: END ----------------//
+
+  // ----------------- Sub Task as Accordion Body: START ----------------//
+  let accordionBodyWrapper = document.createElement("div");
+  accordionBodyWrapper.id = "collapse_taskgroup" + task.group_id;
+  let showBody = false ? "show" : "collapse";
+  accordionBodyWrapper.className = "accordion-collapse collapse " + showBody;
+  accordionBodyWrapper.setAttribute(
+    "aria-labelledby",
+    "group_task_heading" + task.group_id
+  );
+  accordionBodyWrapper.setAttribute(
+    "data-bs-parent",
+    `group_id${task.group_id}`
+  );
+  let accordionBody = document.createElement("div");
+  accordionBody.className = "accordion-body";
+
+  // ------------- Build Nested Table: START ----------------- //
+  {
+    let table = document.createElement("table");
+    table.className = "table table-bordered mb-0";
+    let nestedTbody = document.createElement("tbody");
+
+    tasks.forEach((task) => {
+      let nestedTr = buildNestedTableRow(task);
+      nestedTbody.appendChild(nestedTr);
+    });
+
+    table.appendChild(nestedTbody);
+    accordionBody.appendChild(table);
+  }
+
+  // Build up DOM
+  accordionBodyWrapper.appendChild(accordionBody);
+  accordionItem.appendChild(accordionBodyWrapper);
+  // ----------------- Sub Task as Accordion Body: END ----------------//
+
+  td.appendChild(accordionItem);
+  tr.appendChild(td);
+
+  return tr;
 }
 
 function buildTableRow({
@@ -349,6 +377,60 @@ function buildTableRow({
     td_edit_button.appendChild(createButton("Edit", onEdit, "secondary"));
     tr.appendChild(td_edit_button);
   }
+
+  //     let showSignUpButton = JSON.parse(taskData.show_signupT_cancelF);
+  //     if (showSignUpButton) {
+  //       let td_signup_button = document.createElement("td");
+  //       td_signup_button.appendChild(accordionSignupButton);
+  //       tr.appendChild(td_signup_button);
+  //     }
+
+  //     let showCancelButton =
+  //       !JSON.parse(taskData.show_signupT_cancelF) &&
+  //       JSON.parse(taskData.can_cancel);
+  //     if (showCancelButton) {
+  //       let td_cancel_button = document.createElement("td");
+  //       td_cancel_button.appendChild(accordionCancelButton);
+  //       tr.appendChild(td_signup_button);
+  //     }
+
+  return tr;
+}
+
+function buildNestedTableRow({
+  group_id,
+  program_name,
+  start_time,
+  end_time,
+  task_description,
+  spots_taken,
+  max_users,
+  user_name,
+}) {
+  let tr = document.createElement("tr");
+  tr.className = "text-center";
+  tr.setAttribute("id", `group_id${group_id}`);
+
+  let td_program = buildTableCell(program_name);
+  tr.appendChild(td_program);
+
+  let td_date = buildTableCell(start_time, true);
+  tr.appendChild(td_date);
+
+  let td_start_time = buildTableCell(start_time, false, true);
+  tr.appendChild(td_start_time);
+
+  let td_end_time = buildTableCell(end_time, false, true);
+  tr.appendChild(td_end_time);
+
+  let td_desc = buildTableCell(task_description);
+  tr.appendChild(td_desc);
+
+  let td_spot = buildTableCell(`${spots_taken}/${max_users}`);
+  tr.appendChild(td_spot);
+
+  let td_username = buildTableCell(user_name);
+  tr.appendChild(td_username);
 
   return tr;
 }
