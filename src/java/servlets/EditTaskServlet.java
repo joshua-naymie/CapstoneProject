31e4edf8db8916a417b489a0e2256de396e2f693
agambeer <agambeer.brar@edu.sait.ca>
2022-04-10 11:44:18 -0600

    Updated EditTaskServlet doPost

--- src/java/servlets/EditTaskServlet.java
+++ src/java/servlets/EditTaskServlet.java
@@ -324,10 +324,9 @@ public class EditTaskServlet extends HttpServlet {
 
                 taskService.update(task);
             }
-
-            response.sendRedirect("tasks");
         } catch (Exception ex) {
             Logger.getLogger(TaskServlet.class.getName()).log(Level.WARNING, null, ex);
         }
+        response.sendRedirect("tasks");
     }
 }
