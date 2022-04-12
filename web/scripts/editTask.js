2ef277437515be578f30b3be848c111905ceea55
Li <xiaomeng.li@edu.sait.ca>
2022-04-11 21:08:57 -0600

    FrontEnd Update: Integration on the editTask page

--- web/scripts/editTask.js
+++ web/scripts/editTask.js
@@ -1,41 +1,43 @@
-//var editTask = {"id":"1",
-//		"program_name":"Chinese Emotional Support Hotline",
-//		"max_users":null,
-//		"start_time":"Tue Feb 15 09:30:00 MST 2022",
-//		"end_time":"Tue Feb 15 10:30:00 MST 2022",
-//		"available":"true",
-//		"notes":null,
-//		"task_description":null,
-//		"task_city":Calgary};
+$("#submit").on("click", (e) => onSubmitEditTask(e));
 
-// window.onload = () => {
-console.log(editTask);
-let task_desc = document.getElementById("task_description");
-task_desc.setAttribute("value", editTask.task_description);
+function onSubmitEditTask(e) {
+  e.preventDefault();
 
-let task_program = document.getElementById("task_program");
-// task_program.setAttribute("value", editTask.program_name);
-$("#task_program").append("<option>" + editTask.program_name + "</option>");
+  let task_description = $("#task_descirption").val();
+  let task_program = $("#task_program").val();
+  let task_city = $("#task_city").val();
+  let task_start_time = $("#task_start_time").val();
+  let task_end_time = $("#task_end_time").val();
+  let task_max_users = $("#task_max_users").val();
+  let task_store = $("#storeAdd").val();
+  let task_company = $("#companyAdd").val();
 
-let task_city = document.getElementById("task_city");
-task_city.setAttribute("value", editTask.task_city);
+  let selected_user_id_list = [];
+  $(".selected_users")
+    .children("input")
+    .each(function () {
+      if (this.checked) {
+        selected_user_id_list.push(this.value);
+      }
+    });
+  // console.log(selected_user_id_list);
 
-let task_date = document.getElementById("task_date");
-task_date.setAttribute("value", editTask.date);
-
-let task_start_time = document.getElementById("task_start_time");
-task_start_time.setAttribute("value", editTask.start_time);
-
-let task_end_time = document.getElementById("task_end_time");
-task_end_time.setAttribute("value", editTask.end_time);
-
-let task_available = document.getElementById("task_available");
-task_available.checked = editTask.available;
-
-let task_max_users = document.getElementById("task_max_users");
-task_max_users.setAttribute("value", editTask.max_users);
-console.log(editTask.max_users);
-
-let task_spots_taken = document.getElementById("task_spots_taken");
-task_max_users.setAttribute("value", editTask.spots_taken);
-// };
+  $.ajax({
+    type: "POST",
+    url: "editTask",
+    data: {
+      task_description: task_description,
+      task_program: task_program,
+      task_city: task_city,
+      task_start_time: task_start_time,
+      task_end_time: task_end_time,
+      task_max_users: task_max_users,
+      selected_user_id_list: selected_user_id_list,
+      task_store: task_store,
+      task_company: task_company,
+    },
+    success: () => {
+      // window.location = "tasks";
+    },
+  });
+}
