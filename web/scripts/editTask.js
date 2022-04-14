448d38095d09c2ea046b86e97559bfa6f8cce55a
Li <xiaomeng.li@edu.sait.ca>
2022-04-14 08:59:44 -0600

    Frontend Update: send task_date to backend for editTask page

--- web/scripts/editTask.js
+++ web/scripts/editTask.js
@@ -1,33 +1,31 @@
 window.onload = () => {
-      // console.log(editTask);
-      let task_desc = document.getElementById("task_description");
-      task_desc.setAttribute("value", editTask.task_description);
+  // console.log(editTask);
+  let task_desc = document.getElementById("task_description");
+  task_desc.setAttribute("value", editTask.task_description);
 
-      let task_program = document.getElementById("task_program");
-      // task_program.setAttribute("value", editTask.program_name);
-      $("#task_program").append(
-        "<option>" + editTask.program_name + "</option>"
-      );
-      task_program.setAttribute("value", editTask.program_name);
+  let task_program = document.getElementById("task_program");
+  // task_program.setAttribute("value", editTask.program_name);
+  $("#task_program").append("<option>" + editTask.program_name + "</option>");
+  task_program.setAttribute("value", editTask.program_name);
 
-      let task_city = document.getElementById("task_city");
-      task_city.setAttribute("value", editTask.task_city);
+  let task_city = document.getElementById("task_city");
+  task_city.setAttribute("value", editTask.task_city);
 
-      let task_date = document.getElementById("task_date");
-      task_date.setAttribute("value", editTask.date);
+  let task_date = document.getElementById("task_date");
+  task_date.setAttribute("value", editTask.date);
 
-      let task_start_time = document.getElementById("task_start_time");
-      task_start_time.setAttribute("value", editTask.start_time);
+  let task_start_time = document.getElementById("task_start_time");
+  task_start_time.setAttribute("value", editTask.start_time);
 
-      let task_end_time = document.getElementById("task_end_time");
-      task_end_time.setAttribute("value", editTask.end_time);
+  let task_end_time = document.getElementById("task_end_time");
+  task_end_time.setAttribute("value", editTask.end_time);
 
-      let task_max_users = document.getElementById("task_max_users");
-      task_max_users.setAttribute("value", editTask.max_users);
+  let task_max_users = document.getElementById("task_max_users");
+  task_max_users.setAttribute("value", editTask.max_users);
 
-      let task_spots_taken = document.getElementById("task_spots_taken");
-      task_spots_taken.setAttribute("value", editTask.spots_taken);
-}
+  let task_spots_taken = document.getElementById("task_spots_taken");
+  task_spots_taken.setAttribute("value", editTask.spots_taken);
+};
 
 $("#submit").on("click", (e) => onSubmitEditTask(e));
 
@@ -69,6 +67,7 @@ function onSubmitEditTask(e) {
       selected_user_id_list: selected_user_id_list,
       task_store: task_store,
       task_company: task_company,
+      task_date: task_date,
     },
     success: () => {
       window.location = "tasks";
